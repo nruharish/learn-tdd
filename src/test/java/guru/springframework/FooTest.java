e1dadcce6e084333e6afe048ae4d2e60a714336b
Harish B S <bs.harish@gmail.com>
2025-04-04 13:14:06 +0530

    second commit

---
+++ src/test/java/guru/springframework/FooTest.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FooTest {
+
+    @Test
+    void getBar() {
+        Foo foo = new Foo();
+        assertEquals("FooBar", foo.getBar());
+    }
+}
